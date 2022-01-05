49fd57c50afab39c2ce1bd17120202c143aed98b
roussleo <leon.roussel@grenoble-inp.org>
2022-01-05 10:17:09 +0100

    test: start of shell scripts

---
+++ src/test/script/test-pars.sh
@@ -0,0 +1,88 @@
+#! /bin/sh
+
+# Go in home repository :
+cd "$(dirname "$0")"/../../.. || exit 1
+
+PATH=./src/test/script/launchers:"$PATH"
+
+# ADDED TEST :
+
+# -----------------------------------------------------------------------------
+# Parser Invalid :
+TEST_PARSER_INVALID_PATH="./src/test/deca/syntax/invalid/test_dir/added/parser"
+TEST_PARSER_INVALID_RESULT_PATH="./src/test/deca/syntax/invalid/result_dir/parser"
+nb_correct_invalid=0
+nb_file_invalid=0
+echo "\e[1;36m[BEGIN PARSER INVALID TESTS]\e[1;m"
+
+for i in "$TEST_PARSER_INVALID_PATH"/*.deca
+do
+  nb_file_invalid=$((nb_file_invalid+1))
+  name_test="${i%.*}"
+  name_test="${name_test##*/}"
+
+
+  # Generate output file : (possibly wrong)
+  # test_synt "$i" > "$TEST_LEXER_INVALID_RESULT_PATH"/"$name_test".lis
+
+  grep_result=$(grep -f "$TEST_PARSER_INVALID_RESULT_PATH"/"$name_test".txt "$TEST_PARSER_INVALID_RESULT_PATH"/"$name_test".lis)
+  # echo "Result of grep : $grep_result"
+
+  if ! [ "$grep_result" = "" ]
+    then
+      echo "  \e[1;32m[CORRECT] $name_test\e[1;m"
+      nb_correct_invalid=$((nb_correct_invalid+1))
+    else
+      echo "  \e[1;31m[INCORRECT] $name_test, no match with the error...\e[1;m"
+  fi
+done
+
+echo "\e[1;36m[PARSER INVALID TESTS DONE] Results : $nb_correct_invalid / $nb_file_invalid\e[1;m"
+echo ""
+
+# -----------------------------------------------------------------------------
+# Parser Valid :
+TEST_PARSER_VALID_PATH="./src/test/deca/syntax/valid/test_dir/added/parser"
+TEST_PARSER_VALID_RESULT_PATH="./src/test/deca/syntax/valid/result_dir/parser"
+nb_correct_valid=0
+nb_file_valid=0
+echo "\e[1;36m[BEGIN PARSER VALID TESTS]\e[1;m"
+
+for i in "$TEST_PARSER_VALID_PATH"/*.deca
+do
+  nb_file_valid=$((nb_file_valid+1))
+  name_test="${i%.*}"
+  name_test="${name_test##*/}"
+  #name_test is the name of the file without path and extension
+
+  # Generate output file :
+  # test_lex "$i" > "$TEST_LEXER_VALID_RESULT_PATH"/"$name_test".lis
+
+  differences=$(diff "$TEST_PARSER_VALID_RESULT_PATH"/"$name_test".txt "$TEST_PARSER_VALID_RESULT_PATH"/"$name_test".lis)
+
+  if [ $? -ne 2 ]
+    then
+    if [ "$differences" = "" ]
+      then
+        echo "  \e[1;32m[CORRECT] $name_test\e[1;m"
+        nb_correct_valid=$((nb_correct_valid+1))
+      else
+        echo "  \e[1;31m[INCORRECT] $name_test, here is the differences : \e[1;m"
+        echo "$differences"
+    fi
+
+    else
+      echo "  \e[1;31m[INCORRECT] $name_test not found ... \e[1;m"
+  fi
+done
+
+echo "\e[1;36m[PARSER VALID TESTS DONE] Results : $nb_correct_valid / $nb_file_valid\e[1;m"
+echo ""
+
+# -----------------------------------------------------------------------------
+if [ "$nb_correct_valid" = "$nb_file_valid" ] && [ "$nb_correct_invalid" = "$nb_file_invalid" ]
+  then
+    exit 0
+  else
+    exit 1
+fi
