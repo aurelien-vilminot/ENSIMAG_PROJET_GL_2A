ddb2fd4e079ae9c924530dc0d1a74d7074ef4329
Damien Clauzon <damien.clauzon@grenoble-inp.org>
2022-01-05 10:59:32 +0100

    feat(DecacCompiler): add destFile, replacing .deca by .ass

--- src/main/java/fr/ensimag/deca/DecacCompiler.java
+++ src/main/java/fr/ensimag/deca/DecacCompiler.java
@@ -125,9 +125,8 @@ public class DecacCompiler {
      */
     public boolean compile() {
         String sourceFile = source.getAbsolutePath();
-        String destFile = null;
-        // A FAIRE: calculer le nom du fichier .ass à partir du nom du
-        // A FAIRE: fichier .deca.
+        // TODO: verify that sourceFile is .deca
+        String destFile = source.getAbsolutePath().replaceAll("\\.deca$", ".ass");
         PrintStream err = System.err;
         PrintStream out = System.out;
         LOG.debug("Compiling file " + sourceFile + " to assembly file " + destFile);
