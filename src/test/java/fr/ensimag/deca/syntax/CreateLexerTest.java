2f7099cdf628ec7b5137c6cea27c4c551148d708
roussleo <leon.roussel@grenoble-inp.org>
2022-01-06 17:42:29 +0100

    test-unit: lexer

--- src/test/java/fr/ensimag/deca/syntax/CreateLexerTest.java
+++ src/test/java/fr/ensimag/deca/syntax/CreateLexerTest.java
@@ -3,12 +3,15 @@ package fr.ensimag.deca.syntax;
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 
 public class CreateLexerTest {
     @Test
-    public void testLexerBadInstantiation(){
+    public void testLexerInstantiationTooMuchArgs(){
         Logger.getRootLogger().setLevel(Level.WARN);
         String [] args = new String[2];
 
@@ -20,7 +23,27 @@ public class CreateLexerTest {
         String actualMessage = exception.getMessage();
 
         assertTrue(actualMessage.contains(expectedMessage));
+    }
 
+    /**
+     * Hard to test because of stdin ...
+     */
+    /*
+    @Test
+    public void testLexerInstantiationTNoArgs () throws IOException {
+        Logger.getRootLogger().setLevel(Level.WARN);
+        String [] args = new String[0];
+        DecaLexer lex = AbstractDecaLexer.createLexerFromArgs(args);
+    }
+    */
+
+    @Test
+    public void testLexerInstantiation() throws IOException{
+        Logger.getRootLogger().setLevel(Level.WARN);
+        String [] args = new String[1];
+        args[0] = "./src/test/deca/syntax/valid/test_dir/provided/hello.deca";
+        DecaLexer lex = AbstractDecaLexer.createLexerFromArgs(args);
+        assertEquals(lex.getSourceName(), args[0]);
     }
 
 }
