245d18c10ea92f3365b2d45efd43463691ef9121
Aurélien VILMINOT <aurelien.vilminot@ensimag.grenoble-inp.org>
2022-01-08 22:17:38 +0100

    feat: instanceOf implementation

--- src/main/java/fr/ensimag/deca/tree/InstanceOf.java
+++ src/main/java/fr/ensimag/deca/tree/InstanceOf.java
@@ -6,13 +6,41 @@ import fr.ensimag.deca.context.ContextualError;
 import fr.ensimag.deca.context.EnvironmentExp;
 import fr.ensimag.deca.context.Type;
 import fr.ensimag.deca.tools.IndentPrintStream;
+import org.apache.commons.lang.Validate;
+import org.apache.log4j.Logger;
 
 import java.io.PrintStream;
 
 public class InstanceOf extends AbstractExpr {
+    private static final Logger LOG = Logger.getLogger(Main.class);
+
+    final private AbstractExpr expr;
+    final private AbstractIdentifier type;
+
+    public InstanceOf(AbstractExpr expr, AbstractIdentifier type) {
+        Validate.notNull(type);
+        Validate.notNull(expr);
+        this.expr = expr;
+        this.type = type;
+    }
+
     @Override
     public Type verifyExpr(DecacCompiler compiler, EnvironmentExp localEnv, ClassDefinition currentClass) throws ContextualError {
-        return null;
+        LOG.debug("verify InstanceOf: start");
+        Validate.notNull(compiler, "Compiler (env_types) object should not be null");
+        Validate.notNull(localEnv, "Env_exp object should not be null");
+
+        // Get expr type
+        Type exprType = this.expr.verifyExpr(compiler, localEnv, currentClass);
+        Type instanceType = this.type.verifyType(compiler);
+
+        if (!exprType.isClass() || !instanceType.isClass()) {
+            throw new ContextualError("InstanceOf works only with two class types operands", this.getLocation());
+        }
+
+        this.setType(instanceType);
+        LOG.debug("verify InstanceOf: end");
+        return instanceType;
     }
 
     @Override
