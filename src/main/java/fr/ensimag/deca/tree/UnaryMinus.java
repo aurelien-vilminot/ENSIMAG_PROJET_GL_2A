cf6b355dc216d46c14a5502af08c874486684b6f
Aurélien VILMINOT <aurelien.vilminot@ensimag.grenoble-inp.org>
2022-01-08 14:40:08 +0100

    feat: context syntax rules for unary and binary expressions

--- src/main/java/fr/ensimag/deca/tree/UnaryMinus.java
+++ src/main/java/fr/ensimag/deca/tree/UnaryMinus.java
@@ -1,16 +1,16 @@
 package fr.ensimag.deca.tree;
 
-import fr.ensimag.deca.context.Type;
+import fr.ensimag.deca.context.*;
 import fr.ensimag.deca.DecacCompiler;
-import fr.ensimag.deca.context.ClassDefinition;
-import fr.ensimag.deca.context.ContextualError;
-import fr.ensimag.deca.context.EnvironmentExp;
+import org.apache.commons.lang.Validate;
+import org.apache.log4j.Logger;
 
 /**
  * @author gl07
  * @date 01/01/2022
  */
 public class UnaryMinus extends AbstractUnaryExpr {
+    private static final Logger LOG = Logger.getLogger(Main.class);
 
     public UnaryMinus(AbstractExpr operand) {
         super(operand);
@@ -19,7 +19,20 @@ public class UnaryMinus extends AbstractUnaryExpr {
     @Override
     public Type verifyExpr(DecacCompiler compiler, EnvironmentExp localEnv,
             ClassDefinition currentClass) throws ContextualError {
-        throw new UnsupportedOperationException("not yet implemented");
+        LOG.debug("verify UnaryMinus: start");
+        Validate.notNull(compiler, "Compiler (env_types) object should not be null");
+        Validate.notNull(localEnv, "Env_exp object should not be null");
+
+        Type typeOperand = this.getOperand().verifyExpr(compiler, localEnv, currentClass);
+
+        if (typeOperand.isInt() || typeOperand.isFloat()) {
+            this.setType(typeOperand);
+        } else {
+            throw new ContextualError("Minus operation is only allowed for int or float types", this.getLocation());
+        }
+
+        LOG.debug("verify UnaryMinus: end");
+        return typeOperand;
     }
 
 
