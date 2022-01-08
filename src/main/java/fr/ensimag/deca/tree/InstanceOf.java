193ace7650f62ad80acb00773afbd780836c0a19
Aurélien VILMINOT <aurelien.vilminot@ensimag.grenoble-inp.org>
2022-01-08 22:17:38 +0100

    feat(parser): Cast + InstanceOf

---
+++ src/main/java/fr/ensimag/deca/tree/InstanceOf.java
@@ -0,0 +1,43 @@
+package fr.ensimag.deca.tree;
+
+import fr.ensimag.deca.DecacCompiler;
+import fr.ensimag.deca.context.ClassDefinition;
+import fr.ensimag.deca.context.ContextualError;
+import fr.ensimag.deca.context.EnvironmentExp;
+import fr.ensimag.deca.context.Type;
+import fr.ensimag.deca.tools.IndentPrintStream;
+import org.apache.commons.lang.Validate;
+
+import java.io.PrintStream;
+
+public class InstanceOf extends AbstractExpr {
+    final private AbstractExpr expr;
+    final private AbstractIdentifier type;
+
+    public InstanceOf(AbstractExpr expr, AbstractIdentifier type) {
+        Validate.notNull(type);
+        Validate.notNull(expr);
+        this.expr = expr;
+        this.type = type;
+    }
+
+    @Override
+    public Type verifyExpr(DecacCompiler compiler, EnvironmentExp localEnv, ClassDefinition currentClass) throws ContextualError {
+        throw new UnsupportedOperationException("not yet implemented");
+    }
+
+    @Override
+    public void decompile(IndentPrintStream s) {
+        throw new UnsupportedOperationException("not yet implemented");
+    }
+
+    @Override
+    protected void prettyPrintChildren(PrintStream s, String prefix) {
+
+    }
+
+    @Override
+    protected void iterChildren(TreeFunction f) {
+
+    }
+}
