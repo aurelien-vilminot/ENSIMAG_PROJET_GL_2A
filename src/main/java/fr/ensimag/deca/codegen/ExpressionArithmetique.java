4f30b202cf4af6d7007be1045eef6efc240a58e2
Aurélien VILMINOT <aurelien.vilminot@ensimag.grenoble-inp.org>
2022-01-10 15:43:21 +0100

    feat: expression arithmetic/atomic

--- src/main/java/fr/ensimag/deca/codegen/ExpressionArithmetique.java
+++ src/main/java/fr/ensimag/deca/codegen/ExpressionArithmetique.java
@@ -1,24 +1,17 @@
 package fr.ensimag.deca.codegen;
 
-import fr.ensimag.ima.pseudocode.DVal;
-import fr.ensimag.ima.pseudocode.GPRegister;
-import fr.ensimag.ima.pseudocode.Instruction;
-import fr.ensimag.ima.pseudocode.instructions.ADD;
-import fr.ensimag.ima.pseudocode.instructions.DIV;
-import fr.ensimag.ima.pseudocode.instructions.MUL;
-import fr.ensimag.ima.pseudocode.instructions.SUB;
 
 public class ExpressionArithmetique {
-    public static Instruction mnemo(String operatorName, DVal op1, GPRegister op2) {
+    public static String mnemo(String operatorName) {
         switch (operatorName) {
             case "+":
-                return new ADD(op1, op2);
+                return "ADD";
             case "-":
-                return new SUB(op1, op2);
+                return "SUB";
             case "*":
-                return new MUL(op1, op2);
+                return "MUL";
             case "/":
-                return new DIV(op1, op2);
+                return "DIV";
             default:
                 return null;
         }
--- src/main/java/fr/ensimag/deca/codegen/ExpressionAtomique.java
+++ src/main/java/fr/ensimag/deca/codegen/ExpressionAtomique.java
@@ -1,11 +1,18 @@
 package fr.ensimag.deca.codegen;
 
+import fr.ensimag.deca.DecacCompiler;
 import fr.ensimag.deca.tree.AbstractExpr;
+import fr.ensimag.deca.tree.Identifier;
+import fr.ensimag.deca.tree.IntLiteral;
 import fr.ensimag.ima.pseudocode.Instruction;
 
 public class ExpressionAtomique {
-    public static Instruction dval(AbstractExpr op1) {
-        // TODO
-        return null;
+    public static String dval(IntLiteral n) {
+        return "#" + n.getValue();
+    }
+
+    public static String dval(DecacCompiler compiler, Identifier symb) {
+        //TODO
+        return "@";
     }
 }
