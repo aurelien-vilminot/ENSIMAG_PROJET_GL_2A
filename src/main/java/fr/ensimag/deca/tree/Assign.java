bce261d8d337cd25fd1a514a7a66c8f4fccbb5da
Aurélien VILMINOT <aurelien.vilminot@ensimag.grenoble-inp.org>
2022-01-11 15:37:52 +0100

    fix: tree decoration during float conversion

--- src/main/java/fr/ensimag/deca/tree/Assign.java
+++ src/main/java/fr/ensimag/deca/tree/Assign.java
@@ -46,6 +46,7 @@ public class Assign extends AbstractBinaryExpr {
         if (this.getLeftOperand().getType().isFloat() && this.getRightOperand().getType().isInt()) {
             // Implicit float conversion
             this.setRightOperand(new ConvFloat(this.getRightOperand()));
+            this.getRightOperand().verifyExpr(compiler, localEnv, currentClass);
         }
 
         LOG.debug("verify Assign: end");
