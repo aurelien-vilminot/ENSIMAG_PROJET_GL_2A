a7353511cbdbcb11779a201a6a8f8ae910e7bd6b
Damien Clauzon <damien.clauzon@grenoble-inp.org>
2022-01-15 10:03:24 +0100

    feat(parser): create New class

---
+++ src/main/java/fr/ensimag/deca/tree/New.java
@@ -0,0 +1,48 @@
+package fr.ensimag.deca.tree;
+
+import fr.ensimag.deca.DecacCompiler;
+import fr.ensimag.deca.context.ClassDefinition;
+import fr.ensimag.deca.context.ContextualError;
+import fr.ensimag.deca.context.EnvironmentExp;
+import fr.ensimag.deca.context.Type;
+import fr.ensimag.deca.tools.IndentPrintStream;
+import fr.ensimag.ima.pseudocode.Register;
+import fr.ensimag.ima.pseudocode.instructions.*;
+import org.apache.commons.lang.Validate;
+import org.apache.log4j.Logger;
+
+import java.io.PrintStream;
+
+public class New extends AbstractExpr {
+    private static final Logger LOG = Logger.getLogger(Main.class);
+
+    final private AbstractIdentifier ident;
+
+    public New(AbstractIdentifier ident) {
+        Validate.notNull(ident);
+        this.ident = ident;
+    }
+
+    @Override
+    public Type verifyExpr(DecacCompiler compiler, EnvironmentExp localEnv, ClassDefinition currentClass)
+            throws ContextualError {
+        throw new UnsupportedOperationException("not yet implemented");
+    }
+
+    @Override
+    public void decompile(IndentPrintStream s) {
+        s.print("new ");
+        ident.decompile(s);
+        s.print("()");
+    }
+
+    @Override
+    protected void prettyPrintChildren(PrintStream s, String prefix) {
+        ident.prettyPrint(s, prefix, true);
+    }
+
+    @Override
+    protected void iterChildren(TreeFunction f) {
+        ident.iter(f);
+    }
+}
