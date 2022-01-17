76f1ed17ba311b28688e77fc8a19ee9c119f528d
Aurélien VILMINOT <aurelien.vilminot@ensimag.grenoble-inp.org>
2022-01-17 16:35:14 +0100

    fix(context): set fields number for class

--- src/main/java/fr/ensimag/deca/tree/ListDeclField.java
+++ src/main/java/fr/ensimag/deca/tree/ListDeclField.java
@@ -1,6 +1,7 @@
 package fr.ensimag.deca.tree;
 
 import fr.ensimag.deca.DecacCompiler;
+import fr.ensimag.deca.context.ClassDefinition;
 import fr.ensimag.deca.context.ContextualError;
 import fr.ensimag.deca.tools.IndentPrintStream;
 import fr.ensimag.deca.tools.SymbolTable;
@@ -15,6 +16,7 @@ public class ListDeclField extends TreeList<AbstractDeclField> {
         for (AbstractDeclField abstractDeclField : this.getList()) {
             abstractDeclField.verifyDeclField(compiler, superSymbol, symbolCurrentClass);
         }
+        ((ClassDefinition) compiler.getEnvironmentTypes().get(symbolCurrentClass)).setNumberOfFields(this.getList().size());
         LOG.debug("verify listDeclField: end");
     }
 
