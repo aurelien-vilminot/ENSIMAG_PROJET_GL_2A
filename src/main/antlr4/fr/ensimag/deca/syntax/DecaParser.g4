cb8490b87d87d939a0adf68e7d413b75df510293
Damien Clauzon <damien.clauzon@grenoble-inp.org>
2022-01-17 17:16:35 +0100

    feat(codeGen): create Selection

--- src/main/antlr4/fr/ensimag/deca/syntax/DecaParser.g4
+++ src/main/antlr4/fr/ensimag/deca/syntax/DecaParser.g4
@@ -404,6 +404,8 @@ select_expr returns[AbstractExpr tree]
         }
         | /* epsilon */ {
             // we matched "e.i"
+            $tree = new Selection($e1.tree, $i.tree);
+            setLocation($tree, $DOT);
         }
         )
     ;
---
+++ src/main/java/fr/ensimag/deca/tree/Selection.java
@@ -0,0 +1,44 @@
+package fr.ensimag.deca.tree;
+
+import fr.ensimag.deca.DecacCompiler;
+import fr.ensimag.deca.context.ClassDefinition;
+import fr.ensimag.deca.context.ContextualError;
+import fr.ensimag.deca.context.EnvironmentExp;
+import fr.ensimag.deca.context.Type;
+import fr.ensimag.deca.tools.IndentPrintStream;
+import org.apache.commons.lang.Validate;
+
+import java.io.PrintStream;
+
+public class Selection extends AbstractLValue {
+
+    final private AbstractExpr expr;
+    final private AbstractIdentifier ident;
+
+    public Selection(AbstractExpr expr, AbstractIdentifier ident) {
+        Validate.notNull(expr);
+        Validate.notNull(ident);
+        this.expr = expr;
+        this.ident = ident;
+    }
+
+    @Override
+    public Type verifyExpr(DecacCompiler compiler, EnvironmentExp localEnv, ClassDefinition currentClass) throws ContextualError {
+        return null;
+    }
+
+    @Override
+    public void decompile(IndentPrintStream s) {
+
+    }
+
+    @Override
+    protected void prettyPrintChildren(PrintStream s, String prefix) {
+
+    }
+
+    @Override
+    protected void iterChildren(TreeFunction f) {
+
+    }
+}
