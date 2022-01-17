0c261002b4710cc3c090d5abeb41c4835913f80e
Aurélien VILMINOT <aurelien.vilminot@ensimag.grenoble-inp.org>
2022-01-17 17:57:40 +0100

    fix(context): field selection

--- src/main/java/fr/ensimag/deca/tree/Selection.java
+++ src/main/java/fr/ensimag/deca/tree/Selection.java
@@ -4,10 +4,12 @@ import fr.ensimag.deca.DecacCompiler;
 import fr.ensimag.deca.context.*;
 import fr.ensimag.deca.tools.IndentPrintStream;
 import org.apache.commons.lang.Validate;
+import org.apache.log4j.Logger;
 
 import java.io.PrintStream;
 
 public class Selection extends AbstractLValue {
+    private static final Logger LOG = Logger.getLogger(Main.class);
 
     final private AbstractExpr expr;
     final private AbstractIdentifier ident;
@@ -21,33 +23,34 @@ public class Selection extends AbstractLValue {
 
     @Override
     public Type verifyExpr(DecacCompiler compiler, EnvironmentExp localEnv, ClassDefinition currentClass) throws ContextualError {
+        LOG.debug("verify Selection: start");
+
         Type classType = this.expr.verifyExpr(compiler, localEnv, currentClass);
+        Type identType = this.ident.verifyExpr(compiler, ((ClassDefinition)compiler.getEnvironmentTypes().get(classType.getName())).getMembers(), currentClass);
 
         TypeDefinition typeDefinition = compiler.getEnvironmentTypes().get(classType.getName());
         if (typeDefinition == null || !typeDefinition.isClass()) {
             throw new ContextualError("Undefined class", this.getLocation());
         }
 
-        Type identifierType = null;
-
-        if (this.ident.getVisibility() == Visibility.PUBLIC) {
+        if (this.ident.getFieldDefinition().getVisibility() == Visibility.PUBLIC) {
             // Case PUBLIC
-            identifierType = this.ident.verifyType(compiler);
+            this.setType(identType);
+            LOG.debug("verify Selection: end");
+            return identType;
         } else {
             // Case PROTECTED
             boolean isSubClass = compiler.getEnvironmentTypes().subTypes(classType, currentClass.getType());
             boolean isSubClassField = compiler.getEnvironmentTypes().subTypes(classType, this.ident.getFieldDefinition().getContainingClass().getType());
 
             if (isSubClass && isSubClassField) {
-                identifierType = this.ident.verifyType(compiler);
+                this.setType(identType);
+                LOG.debug("verify Selection: end");
+                return identType;
             }
         }
 
-        if (identifierType == null) {
-            throw new ContextualError("A class is not a subtype of an other class", this.getLocation());
-        }
-
-        return identifierType;
+        throw new ContextualError("A class is not a subtype of an other class", this.getLocation());
     }
 
     @Override
