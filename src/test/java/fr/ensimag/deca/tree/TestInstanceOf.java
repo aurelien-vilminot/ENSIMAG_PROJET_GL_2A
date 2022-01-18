d4da382dd42ebc8ca87366d8d4cdaba5c1f58c70
Guilherme Klein Kern <guilherme.klein-kern@grenoble-inp.org>
2022-01-18 16:38:06 +0100

    test(tree): add unit test for InstanceOf and While(draft)

---
+++ src/test/java/fr/ensimag/deca/tree/TestInstanceOf.java
@@ -0,0 +1,45 @@
+package fr.ensimag.deca.tree;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import org.mockito.Mock;
+import static org.mockito.Mockito.*;
+import org.mockito.MockitoAnnotations;
+
+import fr.ensimag.deca.DecacCompiler;
+import fr.ensimag.deca.context.*;
+
+
+public class TestInstanceOf {
+    private DecacCompiler compiler;
+    private EnvironmentExp localEnv;
+
+    @Mock
+    AbstractExpr classExpr;
+    @Mock
+    AbstractIdentifier classId;
+    @Mock
+    Type classType;
+
+    @BeforeEach
+    public void setup() throws ContextualError {
+        MockitoAnnotations.openMocks(this);
+
+        compiler = new DecacCompiler(null, null);
+        localEnv = new EnvironmentExp(null);
+
+        when(classType.isClass()).thenReturn(true);
+        when(classType.isNull()).thenReturn(false);
+        when(classExpr.verifyExpr(compiler, localEnv, null)).thenReturn(classType);
+        when(classId.verifyType(compiler)).thenReturn(classType);
+    }
+
+    @Test
+    public void testVerifyExpr() throws ContextualError {
+        InstanceOf i = new InstanceOf(classExpr, classId);
+        i.verifyExpr(compiler, localEnv, null);
+        assertTrue(i.getType().isBoolean());
+    }
+}
--- src/test/java/fr/ensimag/deca/tree/TestWhile.java
+++ src/test/java/fr/ensimag/deca/tree/TestWhile.java
@@ -2,19 +2,37 @@ package fr.ensimag.deca.tree;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import fr.ensimag.deca.DecacCompiler;
+import fr.ensimag.deca.context.ContextualError;
+import fr.ensimag.deca.context.EnvironmentExp;
+import fr.ensimag.deca.context.Type;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeEach;
 
+import org.mockito.Mock;
+import static org.mockito.Mockito.*;
+import org.mockito.MockitoAnnotations;
+
 public class TestWhile {
+    private DecacCompiler compiler;
+    private EnvironmentExp localEnv;
     private While w;
     private AbstractExpr condition;
     private ListInst body;
+    @Mock
+    AbstractExpr conditionNonBool;
     
     @BeforeEach
     void setup() {
+        MockitoAnnotations.openMocks(this);
+
+        compiler = new DecacCompiler(null, null);
+        localEnv = new EnvironmentExp(null);
         condition = new BooleanLiteral(true);
         body = new ListInst();
         w = new While(condition, body);
+
+        when(conditionNonBool.getType().isBoolean()).thenReturn(false);
     }
 
     @Test
@@ -25,5 +43,18 @@ public class TestWhile {
     @Test
     public void testGetBody() {
         assertEquals(body, w.getBody());
+    }
+
+    //@Test
+    public void testVerifyInst() {
+        w = new While(conditionNonBool, body);
+
+        Exception exception = assertThrows(ContextualError.class, () -> {
+            w.verifyInst(compiler, localEnv, null, null);
+        });
+
+        String expectedMessage = "The condition must be only boolean type";
+        String actualMessage = exception.getMessage();
+        assertEquals(expectedMessage, actualMessage);
     }
 }
