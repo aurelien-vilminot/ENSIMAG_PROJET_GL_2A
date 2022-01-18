96a96de2fbdc3aa36e7ca524c6c204334927e151
arvyp <pierre.arvy@grenoble-inp.org>
2022-01-18 10:49:02 +0100

    feat(parser): add Java class for Tree grammar

--- src/main/java/fr/ensimag/deca/tree/NewArray.java
+++ src/main/java/fr/ensimag/deca/tree/NewArray.java
@@ -1,3 +1,47 @@
 package fr.ensimag.deca.tree;
+
+import fr.ensimag.deca.DecacCompiler;
+import fr.ensimag.deca.context.*;
+import fr.ensimag.deca.tools.IndentPrintStream;
+import fr.ensimag.ima.pseudocode.DAddr;
+import fr.ensimag.ima.pseudocode.Register;
+import fr.ensimag.ima.pseudocode.RegisterOffset;
+import org.apache.commons.lang.Validate;
+import org.apache.log4j.Logger;
+
+import java.io.PrintStream;
+
 // TODO : Trouver la classe dont elle hérite.
-public class NewArray {}
+public class NewArray extends AbstractExpr{
+    final private AbstractIdentifier type;
+    final private ListExpr indexList;
+
+    public NewArray(AbstractIdentifier type, ListExpr indexList){
+        Validate.notNull(type);
+        Validate.notNull(indexList);
+        this.type = type;
+        this.indexList = indexList;
+    }
+
+    // TODO
+    public void decompile(IndentPrintStream s){
+
+    }
+
+    protected void iterChildren(TreeFunction f){
+
+    }
+
+    protected void prettyPrintChildren(PrintStream s, String prefix){
+
+    }
+
+
+
+
+    public Type verifyExpr(DecacCompiler compiler,
+                                    EnvironmentExp localEnv, ClassDefinition currentClass){
+        return null;
+    }
+
+}
