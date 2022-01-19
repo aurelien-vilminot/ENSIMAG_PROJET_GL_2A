a8654761f62225dfb362971e95364a2ffbc151f1
arvyp <pierre.arvy@grenoble-inp.org>
2022-01-19 11:16:54 +0100

    feat(extension): beginning of ArrayAccess verify function

--- src/main/java/fr/ensimag/deca/tree/ArrayAccess.java
+++ src/main/java/fr/ensimag/deca/tree/ArrayAccess.java
@@ -8,6 +8,7 @@ import fr.ensimag.ima.pseudocode.Register;
 import fr.ensimag.ima.pseudocode.RegisterOffset;
 import org.apache.commons.lang.Validate;
 import org.apache.log4j.Logger;
+import sun.jvm.hotspot.types.WrongTypeException;
 
 import java.io.PrintStream;
 
@@ -15,6 +16,7 @@ import java.io.PrintStream;
 public class ArrayAccess extends AbstractLValue{
     private final AbstractExpr tab;
     private final AbstractExpr index;
+    private static final Logger LOG = Logger.getLogger(Main.class);
 
     public ArrayAccess(AbstractExpr tab, AbstractExpr index){
             Validate.notNull(tab);
@@ -45,6 +47,28 @@ public class ArrayAccess extends AbstractLValue{
 
     public Type verifyExpr(DecacCompiler compiler,
                            EnvironmentExp localEnv, ClassDefinition currentClass){
-        return null;
+        LOG.debug("verify BooleanLiteral: start");
+        Validate.notNull(compiler, "Compiler (env_types) object should not be null");
+//        Validate.notNull(localEnv, "Env_exp object should not be null");
+
+        if(!index.getType().isInt()){
+            throw new WrongTypeException("The index of an ArrayAccess must be an integer");
+        } else {
+
+            if (tab.getType().isMatrixFloat()) {
+
+            } else if (tab.getType().isVectorFloat()) {
+
+            } else if (tab.getType().isMatrixInt()){
+
+            } else if (tab.getType().isVectorInt()){
+
+            } else {
+                throw new WrongTypeException("Attribut 'tab' must be a Vector or a Matrix of Int or Float");
+            }
+
+                Type elementAccessed = compiler.getEnvironmentTypes().get(compiler.getSymbolTable().create("boolean")).getType();
+        }
+        throw new WrongTypeException("Attribut 'tab' must be a Vector or a Matrix of Int or Float");
     }
 }
