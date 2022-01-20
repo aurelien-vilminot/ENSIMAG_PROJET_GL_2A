e3fc65cdc8bd22372f967c9a711db493cb5fbe45
Aurélien VILMINOT <aurelien.vilminot@ensimag.grenoble-inp.org>
2022-01-20 10:48:51 +0100

    fix(context): impossible to get a protected field in the main program

--- src/main/java/fr/ensimag/deca/tree/Selection.java
+++ src/main/java/fr/ensimag/deca/tree/Selection.java
@@ -42,6 +42,12 @@ public class Selection extends AbstractLValue {
             return identType;
         } else {
             // Case PROTECTED
+            if (currentClass == null) {
+                // Cannot access to protected field in the main program
+                throw new ContextualError(
+                        "The identifier '" + this.ident.getName() + "' is protected and it is impossible to access it in the main program",
+                        this.getLocation());
+            }
             boolean isSubClass = compiler.getEnvironmentTypes().subTypes(classType, currentClass.getType());
             boolean isSubClassField = compiler.getEnvironmentTypes().subTypes(classType, this.ident.getFieldDefinition().getContainingClass().getType());
 
