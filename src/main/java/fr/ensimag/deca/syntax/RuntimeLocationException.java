1c1dbdece4670898f631d17c41cda7caf77469c5
Damien Clauzon <damien.clauzon@grenoble-inp.org>
2022-01-23 10:53:46 +0100

    fix(parser): replace RuntimeLocationException by DecaNumberFormatException

--- src/main/java/fr/ensimag/deca/syntax/RuntimeLocationException.java
+++
@@ -1,9 +0,0 @@
-package fr.ensimag.deca.syntax;
-
-import fr.ensimag.deca.tree.Location;
-
-public class RuntimeLocationException extends RuntimeException {
-    public RuntimeLocationException(String message, Location location) {
-        super(location.getFilename() + ":" + Integer.toString(location.getLine()) + ":" + location.getPositionInLine() + ": " + message);
-    }
-}
